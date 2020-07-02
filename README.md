446a2f9def8328dd63a175027b61334b352256fd
Kenny Ballou <kballou@devnulllabs.io>
2020-07-02 06:28:58 -0600

    backup.sh: initial commit
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

---
+++ README.md
@@ -0,0 +1,22 @@
+# backup.sh #
+
+A script that performs configurable backups using basic GNU/Linux tools.  This
+script has a goal of being slightly configurable, but in all likelihood is
+opinionated  towards the author's needs.
+
+# License #
+
+Copyright © 2020 Kenny Ballou
+
+devnulllabs/backup.sh is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+devnulllabs/backup.sh is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+Foobar.  If not, see <https://www.gnu.org/licenses/>.
---
+++ backup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# devnulllabs/backup.sh Copyright 🄯 2020 Kenny Ballou /dev/null labs
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# devnulllabs/backup.sh is a simple script that uses various existing utilities
+# in combination to create a series of backups.  The goal of the script is to
+# be configurable, however, the needs of this program likely mirror that of the
+# authors.
