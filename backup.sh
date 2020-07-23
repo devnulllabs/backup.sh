b440024b5c9d7d93760e8de539ac416d773762a0
Kenny Ballou <kballou@devnulllabs.io>
2020-07-22 23:58:04 -0600

    backup.sh: add remote host backups
    Signed-off-by: Kenny Ballou <kballou@devnulllabs.io>

--- backup.sh
+++ backup.sh
@@ -19,3 +19,84 @@
 # in combination to create a series of backups.  The goal of the script is to
 # be configurable, however, the needs of this program likely mirror that of the
 # authors.
+
+VERBOSE="false"
+FULL="false"
+DESTINATION_DIRECTORY=""
+TARGET_DIRECTORY=""
+REMOTE_HOST=""
+GPG_KEY_ID="${GPG_KEY_ID:-}"
+EXCLUDES_FILE="${HOME}/.config/backup.sh/exclude-patterns"
+SNAPSHOT_DIRECTORY="${HOME}/.config/backup.sh/snapshots/"
+
+function usage() {
+    echo "backup.sh [-v] [-f] [-h <remote host>] -t <target folder> -d <destination>"
+}
+
+function error() {
+    printf "%s\n" "${1}" 1>&2
+}
+
+function parse_arguments() {
+    while getopts 'vfh:d:t:' flag; do
+        case "${flag}" in
+            v) VERBOSE='true' ;;
+            f) FULL='true' ;;
+            t) TARGET_DIRECTORY="${OPTARG}" ;;
+            d) DESTINATION_DIRECTORY="${OPTARG}" ;;
+            h) REMOTE_HOST="${OPTARG}" ;;
+            *)
+                error "Invalid argument/option passed"
+                usage
+                exit 1
+                ;;
+        esac
+    done
+    readonly VERBOSE
+    readonly FULL
+    readonly DESTINATION_DIRECTORY
+    readonly TARGET_DIRECTORY
+    readonly REMOTE_HOST
+}
+
+function backup_to_remote_host() {
+    local snapshot_dir
+    local snapshot_file
+    local snapshot_basename
+    local status
+    snapshot_basename="${TARGET_DIRECTORY/${HOME}\//}"
+    snapshot_dir="${SNAPSHOT_DIRECTORY}${snapshot_basename}"
+    snapshot_file="${snapshot_dir}.snapshot"
+    if [[ "${VERBOSE}" == "true" ]]; then
+        status="progress"
+    else
+        status="none"
+    fi
+    mkdir -p "${snapshot_dir}"
+    if [[ "${FULL}" == "true" && -f "${snapshot_file}" ]]; then
+        rm "${snapshot_file}"
+        touch "${snapshot_file}"
+    fi
+    ssh "${REMOTE_HOST}" mkdir -p "${DESTINATION_DIRECTORY}/${snapshot_basename}"
+    tar -zcg "${snapshot_file}" --exclude-from="${EXCLUDES_FILE}" "${TARGET_DIRECTORY}" | \
+         gpg --encrypt --recipient="${GPG_KEY_ID}" | \
+         ssh "${REMOTE_HOST}" dd of="${DESTINATION_DIRECTORY}/${snapshot_basename}-$(date --iso-8601=minutes).tar.gz.gpg" status="${status}"
+}
+
+function main() {
+    parse_arguments "$@"
+    if [[ -z "${GPG_KEY_ID}" ]]; then
+        error "No GPG key ID set"
+        exit 2
+    fi
+    if [[ -z "${TARGET_DIRECTORY}" || -z "${DESTINATION_DIRECTORY}" ]]; then
+        error "Missing required arguments"
+        usage
+        exit 1
+    fi
+    if [[ -n "${REMOTE_HOST}" ]]; then
+        backup_to_remote_host
+    fi
+}
+
+main "$@"
